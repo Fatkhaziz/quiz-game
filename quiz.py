6c6d2a8bc92a07b0b8590ef8387cb9a1c00ad7eb
Shuhrat Muhamedov <shuhratmuhamedov@MacBook-Pro-Shuhrat.local>
2022-11-20 23:50:17 +0600

    completed the game

--- quiz.py
+++ quiz.py
@@ -21,14 +21,14 @@ def new_game():
             print(el)
         print("__________")
         guess = input("select answer (A, B, C, D)").upper()
-        # for i in guess:
-        #     if i != "ABCD":
-        #         print("out of range")
+        while guess not in "ABCD" or guess == '':
+            guess = input("Answer is out of range. select answer (A, B, C, D)").upper()
         guesses.append(guess)
         print(guesses)
         correct_answers += check_answer(questions.get(key), guess)
         question_num += 1
     display_score(correct_answers, len(guesses))
+
 
 def check_answer(answer, guess):
     if answer == guess:
@@ -43,20 +43,51 @@ def display_score(correct_ans, guesses):
     total = (correct_ans / guesses) * 100
     print("your total score is:", total, "%")
 
+
+def admin_mode():
+    while True:
+        change_mode = input("press 'space' three times to get access to the admin mode")
+        if change_mode == "   ":
+            print("You are in admin mode now!")
+            choice = input("Create a new question? (YES or NO)").upper()
+            if choice != "YES" and choice != "NO":
+                print("Enter only YES or NO")
+            elif choice == "YES":
+                new_question = input("Enter the new question:").upper()
+                new_options = []
+                options_abcd = "ABCD"
+                for i in range(4):
+                    new_option = input("Enter the new option:")
+                    new_options.append(options_abcd[i] + ". " + new_option)
+                options.append(new_options)
+                while True:
+                    option_for_new_question = input(
+                        "Enter the correct option for new question (A, B, C, D):").upper()
+                    if option_for_new_question != "A" and option_for_new_question != "B" and option_for_new_question != "C" and option_for_new_question != "D":
+                        print("Enter only A, B, C, D")
+                    else:
+                        break
+                questions.setdefault(new_question, option_for_new_question.upper())
+            else:
+                print("Bye")
+                break
+        
+
 def play_again():
-    response = input("Do you want to play again: (yes or no)").upper()
+    response = input("Do you want to play?: (yes, no or enter admin mode)").upper()
     if response == "YES":
         return True
     elif response == "NO":
         return False
+    elif response == "ADMIN MODE":
+        return admin_mode()
+
+
 
 def main():
     while play_again():
         new_game()
     print("Bye Bye")
-
-
-def add_new_ques():
 
 
 main()
